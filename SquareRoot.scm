927eceac3e0dcfb46064dc671b686685aa145e13
GautamSingh6599 <gautam6599123@gmail.com>
2020-05-08 19:52:05 +0530

    Completed  Code.

--- SquareRoot.scm
+++ SquareRoot.scm
@@ -1,10 +1,18 @@
-(define square (
-                    lambda (x)   (* x x)))
-(define average (
-                    lambda (x y) (* (+ x y) 0.5))) 
+(define square      (
+                    lambda  (x)   (* x x)))
+(define average     (
+                    lambda  (x y) (* (+ x y) 0.5))) 
 (define close-enuf? (
-                    lambda (g x)
+                    lambda  (g x)
                                  (< abs(- (square g) x) 0.001)))
-(define improve (
-                    lambda (g x)
+(define improve     (
+                    lambda  (g x)
                                  (average g (/ x g))))
+(define sqrt-loop   (
+                    lambda  (g x)
+					             (if (close-enuf g x)
+								     (g)
+									 (sqrt-loop (improve g x) x))))
+(define	sqrt 		(						 
+                     lambda (x)
+					             (sqrt-loop 1.0 x)) 
